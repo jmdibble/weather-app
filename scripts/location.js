f6e7901b8db2e05f1e679dc8b66275aa2f6374a0
Josh <joshdibble93@gmail.com>
2019-03-16 14:52:38 +0000

    Fixed geolocation for new, more robust API

--- scripts/location.js
+++ scripts/location.js
@@ -2,37 +2,24 @@
 
 var x = document.getElementById("btnLocation");
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        console.log("Geolocation is not supported by this browser.");
-    }
-}
-
-function showPosition(position) {
-    let x = position.coords.latitude;
-    let y = position.coords.longitude;
-    console.log(y, x)
-    getCity(y, x);
-}
-
-function getCity(y, x) {
-    let url = `https://api.postcodes.io/outcodes?lon=${y}&lat=${x}`
+function getIP() {
+    let url = `https://ipapi.co/json/ `
     fetch(url)
         .then((response) => {
             return response.json()
         })
         .then((data) => {
             console.log(data)
-            let city1 = (data.result[0].admin_district[0])
-            let country1 = (data.result[0].country[0])
+            let city1 = (data.city)
+            let country1 = (data.country_name)
             city.value = city1
             country.value = country1
         })
 }
 
-btnLocation.addEventListener('click', showPosition)
+
+
+btnLocation.addEventListener('click', getIP)
 
 
 
